2027f8e685029b52cd3ca5cf8d8ebb4f0b5f8a2c
nkmk <nkmk.on@gmail.com>
2025-05-24 10:48:35 +0900

    add new commands

---
+++ src/my_uv_tool/cli.py
@@ -0,0 +1,6 @@
+from my_uv_tool import utils
+
+
+def main():
+    print('Hello from cli.py!')
+    print(f'NumPy version: {utils.numpy_version()}')
---
+++ src/my_uv_tool/utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def numpy_version():
+    return np.__version__
